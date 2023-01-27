f39704970e129956a61c2f19113af8db243ea17d
Hyeongwoo MOON <moc9058@gmail.com>
2023-01-27 16:29:30 +0900

    nth

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import cv2
 import numpy as np
 
-video = cv2.VideoCapture(0)
-ret, image = video.read()
-print(ret)
-# cv2.imshow("Test image",image)
-# print(image.shape)
-# cv2.waitKey(0)
-# video.release()
-# cv2.destroyAllWindows()
+video = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+while True:
+    ret, image = video.read()
+    cv2.imshow("Test image",image)
+    print(image.shape)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+video.release()
+cv2.destroyAllWindows()
